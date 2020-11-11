08b7f9f165d08b6e972877ab5a30466142a5add1
Youngho Cho <dominoyh5@gmail.com>
2020-11-11 19:41:37 +0900

    ch08 complete

--- 01.Kernel32/Source/Main.c
+++ 01.Kernel32/Source/Main.c
@@ -2,17 +2,38 @@
 
 void kPrintString( int iX, int iY, const char* pcString );
 BOOL kInitializeKernel64Area( void );
+BOOL kIsMemoryEnough( void );
 
 void Main( void ) {
     DWORD i;
 
-    kPrintString( 0, 3, "C Language Kernel Started!!" );
+    kPrintString( 0, 3, "C Language Kernel Start...................[PASS]" );
 
-    kInitializeKernel64Area();
-    kPrintString( 0, 4, "IA-32e Kernel Area Initialization Complete" );
+    kPrintString( 0, 4, "Minimum Memory Size Check.................[    ]" );
+    if( kIsMemoryEnough() == FALSE )
+    {
+        kPrintString( 43, 4, "FAIL" );
+        kPrintString( 0, 5, "Not Enough Memory!! MINT64 OS Requires Over 64Mbyte Memory!!" );
+        while( 1 );
+
+    }
+    else
+    {
+        kPrintString( 43, 4, "PASS");
+    }
+    
+    kPrintString( 0, 5, "IA-32e Kernel Area Initialization.........[    ]" );
+    if( kInitializeKernel64Area() == FALSE )
+    {
+        kPrintString( 43, 5, "FAIL");
+        kPrintString( 0, 6, "Kernel Area Initialization Fail!!" );
+        while( 1 );
+    }
+    kPrintString( 43, 5, "PASS" );
 
     while(1);
 }
+
 void kPrintString( int iX, int iY, const char* pcString ) {
     CHARACTER* pstScreen = ( CHARACTER* ) 0xB8000;
     int i;
@@ -41,5 +62,21 @@ BOOL kInitializeKernel64Area( void )
         pdwCurrentAdress++;
     }
 
+    return TRUE;
+}
+
+BOOL kIsMemoryEnough( void )
+{
+    DWORD* pdwCurrentAddress = ( DWORD* ) 0x100000;
+
+    while( ( DWORD ) pdwCurrentAddress < 0x4000000 )
+    {
+        *pdwCurrentAddress = 0x12345678;
+        if( *pdwCurrentAddress != 0x12345678 )
+        {
+            return FALSE;
+        }
+        pdwCurrentAddress += ( 0x100000 / 4 );
+    }
     return TRUE;
 }
